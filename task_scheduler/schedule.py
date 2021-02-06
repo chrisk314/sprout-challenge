21dd3ec2a21b1e644c64190c50e1bdd8d0383e96
Chris Knight <chrisk314@gmail.com>
2021-02-06 14:23:41 +0000

    Adds function to schedule tasks by type (q2)

--- task_scheduler/schedule.py
+++ task_scheduler/schedule.py
@@ -1,5 +1,8 @@
 """Functions/class for schedling worker tasks."""
 
+import pandas as pd
+
+
 def schedule_uniform(tasks, workers):
     """Returns pd.DataFrame with tasks uniformly distributed across workers.
 
@@ -31,5 +34,55 @@ def schedule_uniform(tasks, workers):
         idx_block = unassigned_index[prev_idx:prev_idx + n_tasks]
         tasks.loc[idx_block, 'user'] = w
         prev_idx += n_tasks
+
+    return tasks
+
+
+def fix_incorrect_task_assignments(tasks, workers):
+    """Returns pd.DataFrame of tasks with any incorrectly assigned tasks unassigned.
+
+    Args:
+        tasks: pd.DataFrame containing tasks.
+        workers: dict containing info about workers e.g. {worker: type}
+
+    Returns:
+        pd.DataFrame with incorrectly assigned tasks unassigned.
+    """
+    # First unassign any tasks which are incorrectly assigned for workers of specific type
+    for task_type in tasks.type.unique():
+        workers_for_type = set([
+            w for w, w_type in workers.items()
+            if w_type == task_type
+        ])
+        pre_assigned_workers_for_type = set(tasks[tasks.type == task_type].user.unique())
+        for misassigned_w in pre_assigned_workers_for_type - workers_for_type:
+            tasks.loc[(tasks.type == task_type) & (tasks.user == misassigned_w), 'user'] = 'None'
+    return tasks
+
+
+def schedule_uniform_by_type(tasks, workers):
+    """Returns pd.DataFrame with tasks uniformly distributed across workers by type.
+
+    Args:
+        tasks: pd.DataFrame containing tasks. Some tasks may be pre assigned.
+            Pre assigned tasks will be respected where correctly assigned by type.
+        workers: dict containing info about workers e.g. {worker: type}
+
+    Returns:
+        pd.DataFrame with all tasks assigned to a worker.
+    """
+    tasks = fix_incorrect_task_assignments(tasks, workers)
+
+    # Construct new tasks DataFrame from sub sets by type
+    tasks_by_type_all = []
+    for task_type in tasks.type.unique():
+        tasks_by_type = schedule_uniform(
+            tasks[tasks.type == task_type].copy(),
+            {w: t for w, t in workers.items() if t == task_type}
+        )
+        tasks_by_type_all += [tasks_by_type]
+    tasks = pd.concat(tasks_by_type_all)
+
+    tasks.sort_values(['version', 'user'], inplace=True)
 
     return tasks
