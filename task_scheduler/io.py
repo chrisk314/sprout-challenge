3db1568b2a2c883d969d35af8f569ad644abfecf
Chris Knight <chrisk314@gmail.com>
2021-02-06 13:11:28 +0000

    Adds io handler functions

---
+++ task_scheduler/io.py
@@ -0,0 +1,28 @@
+"""Input/output handler functions
+"""
+import ast
+import json
+import os
+
+import pandas as pd
+
+
+def load_csv_data(path):
+    """Returns pandas.DataFrame containing data loaded from csv file at `path`.
+
+    This function also sanitizes column names, i.e., strips whitespace.
+    """
+    with open(path, 'r') as f:
+        df = pd.read_csv(f)
+    df.columns = [x.strip() for x in df.columns]
+    return df
+
+
+def load_json_data(path):
+    """Returns dict containing data loaded from json file at `path`."""
+    with open(path, 'r') as f:
+        json_s = f.read()
+        try:
+            return json.loads(json_s)
+        except json.JSONDecodeError:
+            return ast.literal_eval(json_s)
