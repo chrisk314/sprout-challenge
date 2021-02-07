174fc518d64ae0c0f47870ae2f773466a9609dc0
Chris Knight <chrisk314@gmail.com>
2021-02-07 14:41:14 +0000

    Adds test for uniform by type scheduler

--- task_scheduler/tests/test_schedule.py
+++ task_scheduler/tests/test_schedule.py
@@ -4,7 +4,7 @@ import os
 from unittest import TestCase
 
 from task_scheduler.io import load_csv_data, load_json_data
-from task_scheduler.schedule import schedule, schedule_uniform
+from task_scheduler.schedule import schedule, schedule_uniform, schedule_uniform_by_type
 
 
 class TestSchedule(TestCase):
@@ -17,6 +17,7 @@ class TestSchedule(TestCase):
         self.workers = load_json_data(self.employees_json_path)
 
     def test_schedule_uniform(self):
+        """Tests schedule.schedule_uniform function."""
         tasks, workers = self.tasks, self.workers
 
         pre_assigned_tasks = {
@@ -24,7 +25,7 @@ class TestSchedule(TestCase):
         }
 
         # tasks = schedule_uniform(tasks, workers)
-        tasks = schedule(tasks, workers, check_assignments=False)
+        tasks = schedule(tasks, workers, quota_func=schedule_uniform, check_assignments=False)
 
         # Check that there are no unassigned tasks
         self.assertEqual(len(tasks[tasks.user == 'None']), 0)
@@ -39,5 +40,36 @@ class TestSchedule(TestCase):
         # For each worker check that all tasks assigned by management are still
         # assigned to only that worker.
         for w, pre_assigned_idx in pre_assigned_tasks.items():
+            now_assigned = set(tasks.loc[pre_assigned_idx, 'user'])
+            self.assertEqual(len(now_assigned - {w}), 0)
+
+    def test_schedule_uniform_by_type(self):
+        """Tests schedule.schedule_uniform_by_type function."""
+        tasks, workers = self.tasks, self.workers
+
+        correctly_pre_assigned_tasks = {
+            w: tasks.index[(tasks.type == t) & (tasks.user == w)] for w, t in workers.items()
+        }
+
+        # tasks = schedule_uniform(tasks, workers)
+        tasks = schedule(tasks, workers, quota_func=schedule_uniform_by_type)
+
+        # Check that there are no unassigned tasks
+        self.assertEqual(len(tasks[tasks.user == 'None']), 0)
+
+        # Check that all workers have been assigned tasks
+        self.assertEqual(set(workers.keys()), set(tasks.user.unique()))
+
+        # Check that number of tasks assigned is uniform by type
+        for task_type in tasks.type.unique():
+            tasks_per_user = [
+                len(tasks[(tasks.type == task_type) & (tasks.user == w)])
+                for w in tasks[tasks.type == task_type].user.unique()
+            ]
+            self.assertLessEqual(max(tasks_per_user) - min(tasks_per_user), 1)
+
+        # For each worker check that all tasks assigned correctly by management are still
+        # assigned to only that worker.
+        for w, pre_assigned_idx in correctly_pre_assigned_tasks.items():
             now_assigned = set(tasks.loc[pre_assigned_idx, 'user'])
             self.assertEqual(len(now_assigned - {w}), 0)
